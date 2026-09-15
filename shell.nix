2367baf4c78300c6a3dc80be7274409b2a0ec2f2
MlgDev <julien.malige@hotmail.fr>
2026-09-15 22:42:08 +0200

    useless files

--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-# Environnement de développement : libpq + ld pour Psycopg, sans flakes.
-# Usage : nix-shell  puis  source .venv/bin/activate
-pkgs.mkShell {
-  packages = [ pkgs.binutils ];
-  LD_LIBRARY_PATH = "${pkgs.postgresql_18.lib}/lib";
-}
